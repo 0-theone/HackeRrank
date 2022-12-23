d1d565b2e48f95cd127c213a9cb4e2e8627ef98a
Raissa Hohenester <raissa.developer@gmail.com>
2022-12-23 23:18:25 +0100

    Add a different method and fix typo

--- minmaxsum.js
+++ minmaxsum.js
@@ -7,7 +7,7 @@
 //The minimum sum is 1 + 3 + 5 + 7 = 16 and the maximum sum is  3 + 5 + 7 + 9 = 24. The function prints
 //16 24
 
-function miniMaxSum(arr) {
+function minMaxSum(arr) {
     const min = Math.min.apply(null, arr);
     const max = Math.max.apply(null, arr);
     const minSum = arr.reduce((a,b) => a + b) - max;
@@ -15,9 +15,23 @@ function miniMaxSum(arr) {
     console.log(minSum,maxSum);
 }
 
+function minMax2(arr) {
+    const sums = [];
+    let i = 0;
+    while (i < arr.length) {
+        const numbers = [...arr];
+        numbers.splice(i, 1);
+        const sum = numbers.reduce((a, b) => a + b);
+        sums.push(sum);
+        console.log(`Sum everything except ${arr[i]}, the sum is ${numbers.join('+')} = ${sum}`);
+        i++;
+    }
+}
+
 function main() {
     const arr = [7, 69, 2, 221, 8974];
-    miniMaxSum(arr);
+    minMaxSum(arr);
+    minMax2(arr);
 }
 
 main();
