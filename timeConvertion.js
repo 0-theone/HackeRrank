a20489ac18046e772b231558197c3c688247588f
Raissa Hohenester <raissa.developer@gmail.com>
2022-12-24 14:52:09 +0100

    Revert

--- timeConvertion.js
+++ timeConvertion.js
@@ -1,10 +1,22 @@
 //Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 
 function timeConversion(s) {
-    let [hours, minutes, seconds] = s.split(':').map((number, index) => index === 0 ? parseInt(number) : number);
-    hours.includes('PM') ? hours != 12 ? hours + 12 : hours :hours == 12 ? '00' : hours.toString().padStart(2, '0');
-    seconds.replace(/\D/g,'');
+    const time = s.split(':').map((number, index) => index === 0 ? parseInt(number) : number);
+    const hours = time[2].includes('PM') ? time[0] != 12 ? time[0] + 12 : time[0] : time[0] == 12 ? '00' : time[0].toString().padStart(2, '0');
+    const minutes = time[1];   
+    const seconds = time[2].replace(/\D/g,'');
     return `${hours}:${minutes}:${seconds}`
+}
+
+function timeConversion(s) {
+    let [hour, minute, second] = s.split(":")
+    if (second[2] === "P" && Number(hour) != 12) {
+        hour = Number(hour) + 12
+    } else if (second[2] === "A" && Number(hour) == 12) {
+        hour = "00"
+    }
+    return `${hour}:${minute}:${second[0]}${second[1]}`
+
 }
 
 function main() {
