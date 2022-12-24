cfa3196808867fd27d12cae53a93440295ecba62
Raissa Hohenester <raissa.developer@gmail.com>
2022-12-24 14:30:24 +0100

    Add time convertion solution

---
+++ timeConvertion.js
@@ -0,0 +1,18 @@
+//Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+function timeConversion(s) {
+    const time = s.split(':').map((number, index) => index === 0 ? parseInt(number) : number);
+    const hours = time[2].includes('PM') ? time[0] != 12 ? time[0] + 12 : time[0] : time[0] == 12 ? '00' : time[0].toString().padStart(2, '0');
+    const minutes = time[1];   
+    const seconds = time[2].replace(/\D/g,'');
+    return `${hours}:${minutes}:${seconds}`
+}
+
+function main() {
+    console.log(timeConversion( "12:45:05PM"))
+    console.log(timeConversion( "12:45:05AM"))
+    console.log(timeConversion( "07:45:05PM"))
+    console.log(timeConversion( "07:45:05AM"))
+}
+
+main();
