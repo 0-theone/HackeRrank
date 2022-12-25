44c6062fa1046304a6f730f7ef5f5205670bbd4a
Raissa Hohenester <raissa.developer@gmail.com>
2022-12-25 13:25:47 +0100

    Add diagonal difference

---
+++ diagonal_difference.js
@@ -0,0 +1,29 @@
+//Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+function calculateDiagonals(matrix) {
+   const n = matrix.length;
+   let leftDiagonalSum = 0;
+   let rightDiagonalSum = 0;
+
+   matrix.map((row, rowIndex) => {
+        row.map((x, itemIndex) => {
+            if(rowIndex === itemIndex) { 
+                leftDiagonalSum += matrix[rowIndex][itemIndex];
+            }
+            if(rowIndex + itemIndex === n - 1){
+                rightDiagonalSum += matrix[rowIndex][itemIndex];
+            }
+        });
+    });
+    console.log(leftDiagonalSum,rightDiagonalSum)
+    return Math.abs(leftDiagonalSum - rightDiagonalSum);
+}
+
+const matrix = [
+  [2, 3,  4,  8], 
+  [5, 3, -1, -5],
+  [9, 8, -2, 12],
+  [2, 3,  4,  8]
+];
+
+console.log(calculateDiagonals(matrix));
